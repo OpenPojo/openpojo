330bdd62a11f84fd70324c6a91f600b216df0d87
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-02-25 18:46:18 +0000

    Adding a new rule for Nested Classes.

---
+++ trunk/src/com/openpojo/validation/rule/impl/NoNestedClassRule.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2010 Osman Shoukry
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.openpojo.validation.rule.impl;
+
+import org.junit.Assert;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.validation.rule.Rule;
+
+/**
+ * This Rule checks for classes being "nested".
+ * 
+ * @author oshoukry
+ */
+public class NoNestedClassRule implements Rule {
+
+    @Override
+    public void evaluate(PojoClass pojoClass) {
+        if (pojoClass.isNestedClass()) {
+            Assert.fail(String.format("Nested Classes not allowed please refactor out to a seperate class [%s]",
+                    pojoClass));
+        }
+    }
+
+}
