3b91dfc078b7eaa262ebc9c4029c0244f408125f
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-04-02 16:37:03 +0000

    Deprecated, and now just routes its calles to GetterMustExistRule & SetterMustExistRule.

--- trunk/src/com/openpojo/validation/rule/impl/GetterSetterMustExistRule.java
+++ trunk/src/com/openpojo/validation/rule/impl/GetterSetterMustExistRule.java
@@ -16,27 +16,22 @@
  */
 package com.openpojo.validation.rule.impl;
 
-import org.junit.Assert;
-
 import com.openpojo.reflection.PojoClass;
-import com.openpojo.reflection.PojoField;
 import com.openpojo.validation.rule.Rule;
-import com.openpojo.validation.utils.ValidationHelper;
 
 /**
  * This rule ensures that all Fields have a getter and setter associated with them.
  * Exception are fields defined static final since those are usually constants.
  * 
  * @author oshoukry
+ * @deprecated Please use individual {@link GetterMustExistRule} & {@link SetterMustExistRule} instead.
  */
+@Deprecated
 public class GetterSetterMustExistRule implements Rule {
 
     @Override
     public void evaluate(PojoClass pojoClass) {
-        for (PojoField fieldEntry : pojoClass.getPojoFields()) {
-            if (!ValidationHelper.isStaticFinal(fieldEntry) && !(fieldEntry.hasGetter() && fieldEntry.hasSetter())) {
-                Assert.fail(fieldEntry + " is missing a getter/setter");
-            }
-        }
+        new GetterMustExistRule().evaluate(pojoClass);
+        new SetterMustExistRule().evaluate(pojoClass);
     }
 }
