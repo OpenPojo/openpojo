4cc9943afe24ed465d46c94468048a9475ff23db
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-04-03 05:09:18 +0000

    Removed GetterSetterMustExistRule and replaced with the independent rules. Migrated to JUnit Assert internal Facade.

--- trunk/example/com/openpojo/PojoTest.java
+++ trunk/example/com/openpojo/PojoTest.java
@@ -18,19 +18,20 @@ package com.openpojo;
 
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.openpojo.reflection.PojoClass;
 import com.openpojo.reflection.impl.PojoClassFactory;
 import com.openpojo.validation.PojoValidator;
+import com.openpojo.validation.affirm.Affirm;
 import com.openpojo.validation.rule.impl.DefaultValuesNullRule;
-import com.openpojo.validation.rule.impl.GetterSetterMustExistRule;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
 import com.openpojo.validation.rule.impl.NoNestedClassRule;
 import com.openpojo.validation.rule.impl.NoPrimitivesRule;
 import com.openpojo.validation.rule.impl.NoPublicFieldsRule;
 import com.openpojo.validation.rule.impl.NoStaticExceptFinalRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
 import com.openpojo.validation.test.impl.GetterTester;
 import com.openpojo.validation.test.impl.SetterTester;
 
@@ -61,7 +62,8 @@ public class PojoTest {
         pojoValidator.addRule(new DefaultValuesNullRule());
         pojoValidator.addRule(new NoPrimitivesRule());
         pojoValidator.addRule(new NoStaticExceptFinalRule());
-        pojoValidator.addRule(new GetterSetterMustExistRule());
+        pojoValidator.addRule(new GetterMustExistRule());
+        pojoValidator.addRule(new SetterMustExistRule());
         pojoValidator.addRule(new NoNestedClassRule());
 
         // Create Testers to validate behaviour for POJO_PACKAGE
@@ -71,7 +73,7 @@ public class PojoTest {
 
     @Test
     public void ensureExpectedPojoCount() {
-        Assert.assertEquals("Classes added / removed?", EXPECTED_CLASS_COUNT, pojoClasses.size());
+        Affirm.affirmEquals("Classes added / removed?", EXPECTED_CLASS_COUNT, pojoClasses.size());
     }
 
     @Test
