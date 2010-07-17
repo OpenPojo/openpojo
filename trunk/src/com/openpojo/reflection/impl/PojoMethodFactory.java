9c43ea03c58e8bf1105e9b30f9cae47713649bc3
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-07-17 04:38:57 +0000

    refactoring the Method lookup responsibility to the PojoMethodFactory. Also made PojoFieldImpl responsible for looking up the getters/setters.

--- trunk/src/com/openpojo/reflection/impl/PojoMethodFactory.java
+++ trunk/src/com/openpojo/reflection/impl/PojoMethodFactory.java
@@ -16,6 +16,7 @@
  */
 package com.openpojo.reflection.impl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
@@ -45,4 +46,89 @@ public class PojoMethodFactory {
         }
         return null;
     }
+
+    /**
+     * Returns the Getter Method for a field.
+     *
+     * @param field
+     *            The field to lookup the getter on.
+     * @return
+     *         The getter method or null if none exist.
+     */
+    public static PojoMethod getFieldGetter(final Field field) {
+        PojoMethod pojoMethod = null;
+        for (String candidateName : generateGetMethodNames(field)) {
+            Class<?> clazz = field.getDeclaringClass();
+            pojoMethod = PojoMethodFactory.getMethod(clazz, candidateName);
+            if (pojoMethod != null) {
+                break;
+            }
+        }
+        return pojoMethod;
+    }
+
+    /**
+     * Returns a list for candidate getter names.
+     *
+     * @param field
+     *            Field to generate the candidate getter names for.
+     * @return
+     *         List of candidate method names.
+     */
+    private static List<String> generateGetMethodNames(final Field field) {
+        List<String> prefix = new LinkedList<String>();
+        prefix.add("get" + formattedFieldName(field));
+        if (field.getType() == boolean.class || field.getType() == Boolean.class) {
+            prefix.add("is" + formattedFieldName(field));
+        }
+        return prefix;
+    }
+
+    /**
+     * Properly CamelCased the field name as expected "first Letter is uppercase, rest is unchanged".
+     *
+     * @param field
+     *            The field to proper case.
+     * @return
+     *         Formatted field name.
+     */
+    private static String formattedFieldName(final Field field) {
+        return field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1, field.getName().length());
+    }
+
+    /**
+     * Returns the Setter Method for a field.
+     *
+     * @param field
+     *            The field to lookup the setter on.
+     * @return
+     *         The setter method or null if none exist.
+     */
+    public static PojoMethod getFieldSetter(final Field field) {
+        PojoMethod pojoMethod = null;
+
+        for (String candidateName : generateSetMethodNames(field)) {
+            Class<?> clazz = field.getDeclaringClass();
+            pojoMethod = PojoMethodFactory.getMethod(clazz, candidateName, field.getType());
+            if (pojoMethod != null) {
+                break;
+            }
+        }
+        return pojoMethod;
+    }
+
+
+    /**
+     * Returns a list for candidate setter names.
+     *
+     * @param field
+     *            The field to generate for.
+     * @return
+     */
+    private static List<String> generateSetMethodNames(final Field field) {
+        List<String> prefix = new LinkedList<String>();
+        prefix.add("set" + formattedFieldName(field));
+        return prefix;
+    }
+
 }
