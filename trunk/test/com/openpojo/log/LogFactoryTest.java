87d9865492920deb6809d51ff633e17c8e3db846
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-07-19 20:32:17 +0000

    Setting the active logger for the test manually. Supressing logging test output from the console while testing.

--- trunk/test/com/openpojo/log/LogFactoryTest.java
+++ trunk/test/com/openpojo/log/LogFactoryTest.java
@@ -6,16 +6,21 @@ import org.junit.Test;
 
 import utils.log.LogHelper;
 
+import com.openpojo.log.impl.Log4JLogger;
+import com.openpojo.log.utils.ActiveLogger;
+
 /**
  * @author oshoukry
  */
 public class LogFactoryTest {
+    Class<? extends Logger> defaultLoggerClass = Log4JLogger.class;
 
     /**
      * This method does the test setup, currently initializes the loggers.
      */
     @Before
     public final void setUp() {
+        ActiveLogger.setActiveLogger(defaultLoggerClass);
         LogHelper.initializeLoggers();
     }
 
@@ -23,13 +28,13 @@ public class LogFactoryTest {
     public final void shouldReturnSLF4JLoggerByClass() {
         Logger log = LoggerFactory.getLogger(LogFactoryTest.class);
         Assert.assertNotNull(log);
-        Assert.assertEquals("com.openpojo.log.impl.SLF4JLogger", log.getClass().getName());
+        Assert.assertEquals(defaultLoggerClass.getName(), log.getClass().getName());
     }
 
     @Test
     public final void shouldReturnSLF4JLoggerByCategory() {
         Logger log = LoggerFactory.getLogger("TestLogger");
         Assert.assertNotNull(log);
-        Assert.assertEquals("com.openpojo.log.impl.SLF4JLogger", log.getClass().getName());
+        Assert.assertEquals(defaultLoggerClass.getName(), log.getClass().getName());
     }
 }
