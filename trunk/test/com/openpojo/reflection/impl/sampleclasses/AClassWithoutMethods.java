637ca6307715a43859f9a8f06d944b1b670a85b5
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-10-19 05:36:46 +0000

    moving project structure to maven default for src and test.

--- trunk/test/com/openpojo/reflection/impl/sampleclasses/AClassWithoutMethods.java
+++
@@ -1,8 +0,0 @@
-package com.openpojo.reflection.impl.sampleclasses;
-
-public class AClassWithoutMethods {
-    @SuppressWarnings("unused")
-    private int privateIntField;
-    public String publicStringField;
-    protected Boolean protectedBoolean;
-}
