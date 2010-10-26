8cc33d3233e2f42cf772c248576705e4602a00d3
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-10-26 23:10:50 +0000

    checking in the registry API...

---
+++ src/test/java/com/openpojo/registry/ServiceRegistrarTest.java
@@ -0,0 +1,27 @@
+package com.openpojo.registry;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ServiceRegistrarTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public final void testRegisterService() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    @Ignore
+    public final void testGetService() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
