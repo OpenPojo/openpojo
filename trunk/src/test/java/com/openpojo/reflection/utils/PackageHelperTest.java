362a673465cf2889d493d358dbeaef99e0abe369
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-11-12 17:21:45 +0000

    Fixed the two tests that needed to be skipped due to clover instrumentation.

--- trunk/src/test/java/com/openpojo/reflection/utils/PackageHelperTest.java
+++ trunk/src/test/java/com/openpojo/reflection/utils/PackageHelperTest.java
@@ -17,7 +17,6 @@
 package com.openpojo.reflection.utils;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -39,10 +38,17 @@ import com.openpojo.reflection.impl.PojoClassFactory;
 public class PackageHelperTest {
     private static final String PACKAGE_NAME = "com.openpojo.utils.dummypackage";
     private static final String[] CLASSES = new String[]{ "Person.class", "Persistable.class", "dummy.txt", "level2" };
+    private static final String CLOVER_STRING = "$__CLR";
 
     @Test
     public final void testGetClasses() {
-        Assert.assertEquals(2, PackageHelper.getClasses(PACKAGE_NAME).size());
+        List<Class<?>> classes = new LinkedList<Class<?>>();
+        for (Class<?> classEntry : PackageHelper.getClasses(PACKAGE_NAME)) {
+            if (!classEntry.getName().contains(CLOVER_STRING)) {
+                classes.add(classEntry);
+            }
+        }
+        Assert.assertEquals(2, classes.size());
     }
 
     /**
@@ -55,13 +61,17 @@ public class PackageHelperTest {
 
         List<String> list = new LinkedList<String>();
         for (File directory : directories) {
-            list.addAll(Arrays.asList(directory.list()));
+            for (String entry : directory.list()) {
+                if (!entry.contains(CLOVER_STRING)) {
+                    list.add(entry);
+                }
+            }
         }
 
         Assert.assertEquals(list.toString(), CLASSES.length, list.size());
         for (String entry : CLASSES) {
             Assert.assertTrue(String.format("expected entry=[%s] in package=[%s] but was not found", entry,
-                    PACKAGE_NAME), list.contains(entry));
+                PACKAGE_NAME), list.contains(entry));
         }
     }
 
@@ -69,19 +79,19 @@ public class PackageHelperTest {
     public final void testClassNotFoundOnClassLoading() {
         PojoClass pojoClass = PojoClassFactory.getPojoClass(PackageHelper.class);
         PojoMethod loadClassPojoMethod = null;
-        for(PojoMethod pojoMethod : pojoClass.getPojoMethods()) {
+        for (PojoMethod pojoMethod : pojoClass.getPojoMethods()) {
             if (pojoMethod.getName().equals("loadClass")) {
-            loadClassPojoMethod = pojoMethod;
+                loadClassPojoMethod = pojoMethod;
             }
         }
         Assert.assertNotNull("loadClass method removed from class PackageHelper.class?", loadClassPojoMethod);
-        //ClassLoader classLoader, final String className
+        // ClassLoader classLoader, final String className
         loadClassPojoMethod.invoke(null, Thread.currentThread().getContextClassLoader(), this.getClass().getName());
 
         // now invoke on non existing class.
         try {
-            loadClassPojoMethod.invoke(null, Thread.currentThread().getContextClassLoader(),
-                    RandomFactory.getRandomValue(String.class));
+            loadClassPojoMethod.invoke(null, Thread.currentThread().getContextClassLoader(), RandomFactory
+                .getRandomValue(String.class));
         } catch (ReflectionException e) {
         }
     }
