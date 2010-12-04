5cae3ef2f34b57cfc031e9892fbb4243ed721016
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2010-12-04 05:12:58 +0000

    [maven-release-plugin]  copy for tag openpojo-0.3.3

---
+++ tags/openpojo-0.3.3/src/main/java/com/openpojo/business/identity/impl/DefaultIdentityEvaluator.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2010 Osman Shoukry
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.openpojo.business.identity.impl;
+
+import com.openpojo.business.annotation.BusinessKey;
+import com.openpojo.business.identity.IdentityEvaluator;
+import com.openpojo.business.utils.BusinessIdentityUtils;
+import com.openpojo.business.utils.BusinessPojoHelper;
+import com.openpojo.reflection.PojoField;
+
+/**
+ * This class is the default implementation for identity evaluation.
+ * The primary function of this class is to evaluate two business entities' equality. <br>
+ * <br>
+ * Note:
+ * if two business fields are defined as optional (aka required = false) and both are set to null,
+ * they are considered equal.
+ *
+ * @author oshoukry
+ */
+public class DefaultIdentityEvaluator implements IdentityEvaluator {
+
+    private DefaultIdentityEvaluator() {
+
+    }
+
+    public static IdentityEvaluator getInstance() {
+        return Instance.INSTANCE;
+    }
+
+    public boolean areEqual(final Object first, final Object second) {
+        if (BusinessIdentityUtils.anyNull(first, second) || !BusinessIdentityUtils.sameClass(first, second)) {
+            return false;
+        }
+
+        if (BusinessIdentityUtils.sameInstance(first, second)) {
+            return true;
+        }
+
+        boolean runningEquality = true;
+        for (PojoField pojoField : BusinessPojoHelper.getBusinessKeyFields(first.getClass())) {
+            BusinessKey businessKey = pojoField.getAnnotation(BusinessKey.class);
+            runningEquality = runningEquality
+                    && BusinessIdentityUtils.areEqual(pojoField, first, second, businessKey.caseSensitive());
+        }
+        return runningEquality;
+    }
+
+    private static class Instance {
+        static final IdentityEvaluator INSTANCE = new DefaultIdentityEvaluator();
+    }
+
+}
---
+++ tags/openpojo-0.3.3/src/test/java/com/openpojo/reflection/filters/FilterEnumTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2010 Osman Shoukry
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.openpojo.reflection.filters;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.PojoClassFilter;
+import com.openpojo.reflection.filters.sampleclasses.SampleEnum;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.affirm.Affirm;
+
+/**
+ * @author oshoukry
+ */
+public class FilterEnumTest {
+
+    private final String sampleClassesPackage = this.getClass().getPackage().getName() + ".sampleclasses";
+    private final PojoClassFilter enumFilter = new FilterEnum();
+    private final List<PojoClass> filteredPojoClasses = PojoClassFactory.getPojoClasses(sampleClassesPackage,
+            enumFilter);
+    private final int EXPECTED_FILTERED_COUNT = 5;
+
+    private final List<PojoClass> nonFilteredPojoClasses = PojoClassFactory.getPojoClasses(sampleClassesPackage);
+    private final int EXPECTED_NON_FILTERED_COUNT = 6;
+
+    @Test
+    public void shouldConfirmSampleClassCount() {
+        Affirm.affirmEquals(String.format("Classes added/removed from [%s]?", sampleClassesPackage),
+                EXPECTED_NON_FILTERED_COUNT, nonFilteredPojoClasses.size());
+        Affirm.affirmEquals(String.format("Classes added/removed from [%s]?", sampleClassesPackage),
+                EXPECTED_FILTERED_COUNT, filteredPojoClasses.size());
+    }
+
+    @Test
+    public void shouldIncludeNonEnum() {
+        for (PojoClass pojoClass : filteredPojoClasses) {
+            Affirm.affirmFalse(String.format("[%s] should have been filtered out!!", pojoClass), pojoClass.isEnum());
+        }
+    }
+
+    @Test
+    public final void shouldExcludeEnum() {
+        Affirm.affirmFalse(String.format("[%s] didn't exclude enum!!", enumFilter), enumFilter.include(PojoClassFactory
+                .getPojoClass(SampleEnum.class)));
+
+    }
+
+}
