76c5cc30165d62b204ab7cf3bef214e978c8777c
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2011-10-10 21:11:46 +0000

    Added one test to validate the generated type counts for collection and map. TODO: add verification for the actual entries one at a time.

--- trunk/src/test/java/com/openpojo/random/collection/CollectionAndMapPackageRandomGeneratorsTest.java
+++ trunk/src/test/java/com/openpojo/random/collection/CollectionAndMapPackageRandomGeneratorsTest.java
@@ -3,6 +3,8 @@ package com.openpojo.random.collection;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,6 +23,8 @@ import com.openpojo.validation.affirm.Affirm;
 public class CollectionAndMapPackageRandomGeneratorsTest {
     private static final List<PojoClass> collectionRandomGenerators = new LinkedList<PojoClass>();
     private static final String[] packages = new String[] { "com.openpojo.random.collection", "com.openpojo.random.map" };
+    private static final int EXPECTED_COUNT = 15;
+    private final Set<Class<?>> expectedCollectionsMaps = new TreeSet<Class<?>>();
 
     @BeforeClass
     public static void setup() {
@@ -28,7 +32,60 @@ public class CollectionAndMapPackageRandomGeneratorsTest {
             collectionRandomGenerators.addAll(PojoClassFactory.getPojoClassesRecursively(pkg, new FilterChain(
                     new RandomGeneratorFilter(), new FilterNestedClasses(), new FilterNonConcrete())));
         }
+        Affirm.affirmEquals(MessageFormatter.format(
+                "Invalid number of Collection/Map RandomGenerators added/removed? found [{0}]",
+                collectionRandomGenerators), EXPECTED_COUNT, collectionRandomGenerators.size());
     }
+
+    /**
+     * TODO: Evaluate this list based on the VM version
+    //@Before
+    @SuppressWarnings("unchecked")
+    public void preTestSetup() {
+        // @formatter:off
+        expectedCollectionsMaps.addAll(Arrays.asList(
+                //Collections
+                Collection.class,
+                AbstractCollection.class,
+
+                //Lists
+                List.class,
+                AbstractList.class,
+                AbstractSequentialList.class,
+                AbstractSet.class,
+                ArrayList.class,
+                LinkedList.class,
+
+                //Maps
+                Map.class,
+                AbstractMap.class,
+                ConcurrentHashMap.class,
+                HashMap.class,
+                Hashtable.class,
+                IdentityHashMap.class,
+                LinkedHashMap.class,
+                SortedMap.class,
+                TreeMap.class,
+                WeakHashMap.class,
+
+                //Sets
+                Set.class,
+                HashSet.class,
+                LinkedHashSet.class,
+                NavigableSet.class,
+                SortedSet.class,
+                TreeSet.class,
+
+                //Queues
+                Queue.class,
+                ArrayDeque.class,
+                DelayQueue.class,
+                LinkedBlockingQueue.class,
+                PriorityBlockingQueue.class,
+                SynchronousQueue.class));
+        //@formatter:on
+    }
+     */
 
     /**
      * This test will test every random generator against its declared types, for every type returned from getTypes.
