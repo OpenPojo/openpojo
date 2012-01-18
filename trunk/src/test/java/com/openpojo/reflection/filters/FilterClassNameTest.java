416ad6bcbdda5a400e92404579f10e00d756a31a
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2012-01-18 22:49:59 +0000

    much better testing for FilterClassName using a PojoClassStub.

--- trunk/src/test/java/com/openpojo/reflection/filters/FilterClassNameTest.java
+++ trunk/src/test/java/com/openpojo/reflection/filters/FilterClassNameTest.java
@@ -16,27 +16,139 @@
  */
 package com.openpojo.reflection.filters;
 
-import com.openpojo.reflection.PojoClassFilter;
-import com.openpojo.reflection.filters.sampleclasses.SampleEnum;
-import com.openpojo.reflection.impl.PojoClassFactory;
-import com.openpojo.validation.affirm.Affirm;
+import java.lang.annotation.Annotation;
+import java.util.List;
+
 import org.junit.Test;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.PojoClassFilter;
+import com.openpojo.reflection.PojoField;
+import com.openpojo.reflection.PojoMethod;
+import com.openpojo.validation.affirm.Affirm;
 
 public class FilterClassNameTest {
 
     @Test
-    public void testShouldIncludeFileName() {
-        PojoClassFilter filter = new FilterClassName(".+Enum");
+    public void testRegExToIncludeClassesNamedTest() {
 
-        Affirm.affirmTrue(String.format("[%s] didn't include class!!", filter), filter.include(PojoClassFactory
-                .getPojoClass(SampleEnum.class)));
+        // Include classNames that have the word Test in the name.
+        final PojoClassFilter filter = new FilterClassName("\\w*Test\\w*$");
+
+        String[] classNames = new String[] { "EndingWithTest", "TestStartingWith", "WithTestInTheMiddleClass",
+                "package.path.TestClass", "package.path.ClassTest", "package.path.ClassTestClass" };
+        final PojoClassStub pojoClassStub = new PojoClassStub();
+
+        for (final String className : classNames) {
+            pojoClassStub.className = className;
+            Affirm.affirmTrue(String.format("[%s] didn't include class [%s]!!", filter, className),
+                              filter.include(pojoClassStub));
+        }
+
+        classNames = new String[] { "package.Test.Class", "TestClass.package" };
+        for (final String className : classNames) {
+            pojoClassStub.className = className;
+            Affirm.affirmFalse(String.format("[%s] didn't exclude class [%s]!!", filter, className),
+                               filter.include(pojoClassStub));
+        }
     }
 
     @Test
-    public void testShouldExncludeFileName() {
-        PojoClassFilter filter = new FilterClassName(".+Sample$");
+    public void testShouldExcludeFileName() {
 
-        Affirm.affirmFalse(String.format("[%s] didn't exclude class!!", filter), filter.include(PojoClassFactory
-                .getPojoClass(SampleEnum.class)));
+        // Include only class names that are in the format of xSample
+        final PojoClassFilter filter = new FilterClassName(".+Sample$");
+
+        final String[] classNames = new String[] { "packagepath.packageSample.MyClass", "SampleClass", "Sample",
+                "sample", "Somesample", "someSampleClass" };
+        final PojoClassStub pojoClassStub = new PojoClassStub();
+
+        for (final String className : classNames) {
+            pojoClassStub.className = className;
+            Affirm.affirmFalse(String.format("[%s] didn't exclude class [%s]!!", filter, className),
+                               filter.include(pojoClassStub));
+        }
+
+    }
+
+    private class PojoClassStub implements PojoClass {
+        private String className;
+
+        public String getName() {
+            return className;
+        }
+
+        public List<? extends Annotation> getAnnotations() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public <T extends Annotation> T getAnnotation(final Class<T> annotationClass) {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean isInterface() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean isAbstract() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean isConcrete() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean isEnum() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean isFinal() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public List<PojoField> getPojoFields() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public List<PojoMethod> getPojoMethods() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public List<PojoMethod> getPojoConstructors() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean extendz(final Class<?> type) {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public PojoClass getSuperClass() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public List<PojoClass> getInterfaces() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public Class<?> getClazz() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public boolean isNestedClass() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public void copy(final Object from, final Object to) {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public String toString(final Object instance) {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
+        public String getSourcePath() {
+            throw new IllegalStateException("UnImplemented!!");
+        }
+
     }
 }
