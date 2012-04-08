8a8ec1aa682e4b9990f63afe4858cb0f4b2d6b42
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2012-04-08 02:18:04 +0000

    Adding a missing test to PojoValidator

--- trunk/src/test/java/com/openpojo/validation/PojoValidatorTest.java
+++ trunk/src/test/java/com/openpojo/validation/PojoValidatorTest.java
@@ -17,13 +17,21 @@
 
 package com.openpojo.validation;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.PojoField;
+import com.openpojo.reflection.PojoMethod;
 import com.openpojo.reflection.impl.PojoClassFactory;
 import com.openpojo.utils.validation.LoggingRule;
 import com.openpojo.utils.validation.LoggingTester;
+import com.openpojo.validation.rule.Rule;
+import com.openpojo.validation.test.Tester;
 
 /**
  * This is a logging tester used for testing.
@@ -34,10 +42,10 @@ public class PojoValidatorTest {
 
     @Test
     public void testRunValidation() {
-        PojoValidator pojoValidator = new PojoValidator();
+        final PojoValidator pojoValidator = new PojoValidator();
 
-        LoggingRule loggingRule = new LoggingRule();
-        LoggingTester loggingTester = new LoggingTester();
+        final LoggingRule loggingRule = new LoggingRule();
+        final LoggingTester loggingTester = new LoggingTester();
 
         pojoValidator.addRule(loggingRule);
         pojoValidator.addTester(loggingTester);
@@ -49,5 +57,114 @@ public class PojoValidatorTest {
 
         Assert.assertEquals(1, loggingRule.getLogs().size());
         Assert.assertEquals(1, loggingTester.getLogs().size());
+    }
+
+    @Test
+    public void shouldEvaluateStructureAndNotRunValidationOnBehaviourSilentlyWithoutExceptions() {
+        final PojoValidator pojoValidator = new PojoValidator();
+        final RuleTesterMock ruleTesterMock = new RuleTesterMock();
+        pojoValidator.addRule(ruleTesterMock);
+        pojoValidator.addTester(new RuleTesterMock());
+        pojoValidator.runValidation(new AbstractPojoClass());
+        Assert.assertTrue("Evaluate not run on non-concrete class", ruleTesterMock.evaluateCalled);
+        Assert.assertTrue("Run called on non-concrete class", ruleTesterMock.runCalled == false);
+    }
+
+    private static class RuleTesterMock implements Rule, Tester {
+        private boolean evaluateCalled = false;
+        private boolean runCalled = false;
+
+        public void evaluate(final PojoClass pojoClass) {
+            evaluateCalled = true;
+        }
+
+        public void run(final PojoClass pojoClass) {
+            runCalled = true;
+        }
+
+    }
+
+    private static class AbstractPojoClass implements PojoClass {
+
+        public String getName() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public List<? extends Annotation> getAnnotations() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public <T extends Annotation> T getAnnotation(final Class<T> annotationClass) {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean isInterface() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean isAbstract() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean isConcrete() {
+            return false;
+        }
+
+        public boolean isEnum() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean isArray() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean isFinal() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public List<PojoField> getPojoFields() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public List<PojoMethod> getPojoMethods() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public List<PojoMethod> getPojoConstructors() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean extendz(final Class<?> type) {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public PojoClass getSuperClass() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public List<PojoClass> getInterfaces() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public Class<?> getClazz() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public boolean isNestedClass() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public void copy(final Object from, final Object to) {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public String toString(final Object instance) {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
+        public String getSourcePath() {
+            throw new IllegalStateException("Unimplemented!!");
+        }
+
     }
 }
