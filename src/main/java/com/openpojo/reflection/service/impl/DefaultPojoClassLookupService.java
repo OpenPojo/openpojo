269111da31df4b1d1584db486dfbfef4eefd637c
oshoukry@gmail.com <oshoukry@gmail.com@00b0ab50-1fee-11df-8328-bd766baa8347>
2012-09-30 03:07:53 +0000

    Updating the copyright information.

--- src/main/java/com/openpojo/reflection/service/impl/DefaultPojoClassLookupService.java
+++ src/main/java/com/openpojo/reflection/service/impl/DefaultPojoClassLookupService.java
@@ -14,6 +14,7 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.openpojo.reflection.service.impl;
 
 import java.util.LinkedList;
