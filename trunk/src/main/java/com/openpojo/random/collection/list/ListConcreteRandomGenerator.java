7735f03df82c694d624bef6119eb3656db928433
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2015-01-20 00:32:14 +0000

    Moving SerializableComparableObject to a more appropriate location.

--- trunk/src/main/java/com/openpojo/random/collection/list/ListConcreteRandomGenerator.java
+++ trunk/src/main/java/com/openpojo/random/collection/list/ListConcreteRandomGenerator.java
@@ -27,7 +27,7 @@ import com.openpojo.random.ParameterizableRandomGenerator;
 import com.openpojo.random.RandomFactory;
 import com.openpojo.random.RandomGenerator;
 import com.openpojo.random.collection.util.CollectionHelper;
-import com.openpojo.random.collection.util.SerializeableComparableObject;
+import com.openpojo.random.util.SerializeableComparableObject;
 import com.openpojo.reflection.Parameterizable;
 import com.openpojo.reflection.construct.InstanceFactory;
 import com.openpojo.reflection.impl.PojoClassFactory;
