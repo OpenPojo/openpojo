b32695667e4d50dd3d01c3c518c8c8c45a73db60
oshoukry <oshoukry@00b0ab50-1fee-11df-8328-bd766baa8347>
2015-02-20 17:12:24 +0000

    Ensuring the EnumRandomGenerator calls the correct values method. Fixed issue 55

---
+++ trunk/src/test/java/com/openpojo/issues/issue55/SomeEnumWithValuesMethod.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2010-2015 Osman Shoukry
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License or any
+ *    later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.openpojo.issues.issue55;
+
+/**
+ * @author oshoukry
+ */
+public enum SomeEnumWithValuesMethod {
+    VALUE1, VALUE2;
+
+    @SuppressWarnings("unused")
+    public static String values(Object ignored) {
+        return "Hello World";
+    }
+}
