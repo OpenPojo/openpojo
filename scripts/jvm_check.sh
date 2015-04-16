9074dab9a37cbfdab5a8e28e5efe763258f8052e
oshoukry <oshoukry@openpojo.com>
2015-04-16 12:28:16 -0700

    Cleaning up variable access on shell script

--- scripts/jvm_check.sh
+++ scripts/jvm_check.sh
@@ -2,13 +2,13 @@
 
 function switch_jdk() {
          local wanted_java_version=$1
-         export JAVA_HOME=`/usr/libexec/java_home -F -v $wanted_java_version -a x86_64 -d64`
+         export JAVA_HOME=`/usr/libexec/java_home -F -v ${wanted_java_version} -a x86_64 -d64`
         
          # cleaned PATH
          export PATH=$(echo $PATH | sed -E "s,(/System)?/Library/Java/JavaVirtualMachines/[a-zA-Z0-9._]+/Contents/Home/bin:,,g")
         
          # prepend wanted JAVA_HOME
-         export PATH=$JAVA_HOME/bin:$PATH
+         export PATH=${JAVA_HOME}/bin:$PATH
         
          echo "Now using : "
          java -version
@@ -19,18 +19,18 @@ LOGFILE=$0.vm.log
 switch_jdk 1.6
 
 pushd ../
-mvn clean package > $LOGFILE 2>&1
-tail $LOGFILE
+mvn clean package > ${LOGFILE} 2>&1
+tail ${LOGFILE}
 
 switch_jdk 1.7
-mvn clean package > $LOGFILE 2>&1
-tail $LOGFILE
+mvn clean package > ${LOGFILE} 2>&1
+tail ${LOGFILE}
 
 switch_jdk 1.8
-mvn clean package > $LOGFILE 2>&1
-tail $LOGFILE
+mvn clean package > ${LOGFILE} 2>&1
+tail ${LOGFILE}
 
 mvn clean
-rm -rf $LOGFILE
+rm -rf ${LOGFILE}
 
 popd
--- scripts/run100.sh
+++ scripts/run100.sh
@@ -2,13 +2,13 @@
 
 function switch_jdk() {
          local wanted_java_version=$1
-         export JAVA_HOME=`/usr/libexec/java_home -F -v $wanted_java_version -a x86_64 -d64`
+         export JAVA_HOME=`/usr/libexec/java_home -F -v ${wanted_java_version} -a x86_64 -d64`
         
          # cleaned PATH
          export PATH=$(echo $PATH | sed -E "s,(/System)?/Library/Java/JavaVirtualMachines/[a-zA-Z0-9._]+/Contents/Home/bin:,,g")
         
          # prepend wanted JAVA_HOME
-         export PATH=$JAVA_HOME/bin:$PATH
+         export PATH=${JAVA_HOME}/bin:$PATH
         
          echo "Now using : "
          java -version
@@ -18,22 +18,22 @@ switch_jdk 1.6
 
 COUNTER=10;
 
-echo "INFO: Running $COUNTER tests back to back"
+echo "INFO: Running ${COUNTER} tests back to back"
 
 pushd ../
 
-while [ $COUNTER -gt 0 ]; do
-  printf "INFO: $COUNTER more to go ..."
-  COUNTER=`expr $COUNTER - 1`
-  mvn clean package > run.$COUNTER
+while [ ${COUNTER} -gt 0 ]; do
+  printf "INFO: ${COUNTER} more to go ..."
+  COUNTER=`expr ${COUNTER} - 1`
+  mvn clean package > run.${COUNTER}
 
-  ISSUCCESSFUL=`cat run.$COUNTER |grep "BUILD SUCCESS" |wc -l`
+  ISSUCCESSFUL=`cat run.${COUNTER} |grep "BUILD SUCCESS" |wc -l`
 
-  if [ $ISSUCCESSFUL -gt 0 ]; then
+  if [ ${ISSUCCESSFUL} -gt 0 ]; then
     echo "PASSED!!"
-    rm -rf run.$COUNTER
+    rm -rf run.${COUNTER}
   else
-    echo "FAILED!! Failure detected in run.$COUNTER, file not removed."
+    echo "FAILED!! Failure detected in run.${COUNTER}, file not removed."
   fi
 
 done
