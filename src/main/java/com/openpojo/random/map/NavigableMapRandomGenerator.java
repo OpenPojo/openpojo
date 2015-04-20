1cd269d640e2774eff54170b4c42e79e875bf1de
oshoukry <oshoukry@openpojo.com>
2015-04-20 10:30:50 -0700

    Adding support for NavigableMap

---
+++ src/main/java/com/openpojo/random/map/NavigableMapRandomGenerator.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2010-2015 Osman Shoukry
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License or any
+ *    later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.openpojo.random.map;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.openpojo.random.exception.RandomGeneratorException;
+import com.openpojo.random.map.util.AbstractMapRandomGenerator;
+import com.openpojo.random.map.util.MapHelper;
+import com.openpojo.random.util.SerializableComparableObject;
+import com.openpojo.reflection.java.load.ClassUtil;
+
+/**
+ * @author oshoukry
+ */
+public class NavigableMapRandomGenerator extends AbstractMapRandomGenerator {
+    private static final String TYPE = "java.util.NavigableMap";
+
+    public static NavigableMapRandomGenerator getInstance() {
+        return Instance.INSTANCE;
+    }
+
+    public Collection<Class<?>> getTypes() {
+        List<Class<?>> types = new ArrayList<Class<?>>();
+        if (ClassUtil.isClassLoaded(TYPE))
+            types.add(ClassUtil.loadClass(TYPE));
+        return types;
+    }
+
+    @Override
+    protected Map getBasicInstance(Class<?> type) {
+        if (!isExpectedType(type))
+            throw RandomGeneratorException.getInstance("Invalid type requested [" + type + "]");
+        return MapHelper.buildMap(new TreeMap(), SerializableComparableObject.class,
+                SerializableComparableObject.class);
+    }
+
+    private NavigableMapRandomGenerator() {
+    }
+
+    private static class Instance {
+        private static final NavigableMapRandomGenerator INSTANCE = new NavigableMapRandomGenerator();
+    }
+}
---
+++ src/test/java/com/openpojo/random/map/NavigableMapRandomGeneratorTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2010-2015 Osman Shoukry
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License or any
+ *    later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.openpojo.random.map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.openpojo.random.ParameterizableRandomGenerator;
+import com.openpojo.random.exception.RandomGeneratorException;
+import com.openpojo.random.map.support.ComparableType1;
+import com.openpojo.random.map.support.ComparableType2;
+import com.openpojo.reflection.java.load.ClassUtil;
+
+/**
+ * @author oshoukry
+ */
+public class NavigableMapRandomGeneratorTest extends AbstractMapRandomGeneratorTest {
+    private static String JVM = System.getProperty("java.class.version");
+    private boolean compatibleVM = Double.valueOf(JVM) > 53.0 ;
+
+    protected ParameterizableRandomGenerator getInstance() {
+        return NavigableMapRandomGenerator.getInstance();
+    }
+
+    protected Class<? extends ParameterizableRandomGenerator> getGeneratorClass() {
+        return NavigableMapRandomGenerator.class;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected Class<? extends Map> getExpectedTypeClass() {
+        return (Class<? extends Map>) ClassUtil.loadClass("java.util.NavigableMap.class");
+    }
+
+    protected Class<? extends Map> getGeneratedTypeClass() {
+        return TreeMap.class;
+    }
+
+    protected Class<?> getGenericType1() {
+        return ComparableType1.class;
+    }
+
+    protected Class<?> getGenericType2() {
+        return ComparableType2.class;
+    }
+
+    public void constructorShouldBePrivate() {
+        super.constructorShouldBePrivate();
+    }
+
+    public void shouldOnlyReturnMapClassFromGetTypes() {
+        if (compatibleVM)
+            super.shouldOnlyReturnMapClassFromGetTypes();
+    }
+
+    public void shouldThrowExceptionForDoGenerateForOtherThanMapClass() {
+        if (compatibleVM)
+            super.shouldThrowExceptionForDoGenerateForOtherThanMapClass();
+        throw RandomGeneratorException.getInstance("Not Suppported VM");
+    }
+
+    public void shouldGenerateParametrizableCorrectMapForRequest() {
+        if (compatibleVM)
+            super.shouldGenerateParametrizableCorrectMapForRequest();
+    }
+
+    public void endToEnd() {
+        if (compatibleVM)
+            super.endToEnd();
+    }
+
+    public void endToEndWithGenerics() {
+        if (compatibleVM)
+            super.endToEndWithGenerics();
+    }
+
+    public void shouldGenerateCorrectTypeMapForRequestedMap() {
+        if (compatibleVM)
+            super.shouldGenerateCorrectTypeMapForRequestedMap();
+    }
+}
