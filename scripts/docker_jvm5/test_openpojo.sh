7be6449d66d59566000d6d1303d0a693a6713022
oshoukry <oshoukry@openpojo.com>
2016-04-17 12:22:38 -0700

    Adding the ability to set the memory prior to test execution. Adding the 64 bit JVM to enable building docker container using 64bit vm.

--- scripts/docker_jvm5/test_openpojo.sh
+++ scripts/docker_jvm5/test_openpojo.sh
@@ -6,5 +6,6 @@ else
   MVN_GOALS="$1" 
 fi
 
-export CMD="cd /openpojo && mvn ${MVN_GOALS} 2>&1"
+JVM_MEM=512m
+export CMD="export MAVEN_OPTS=\"-Xmx${JVM_MEM} -Xms${JVM_MEM}\" && cd /openpojo && mvn ${MVN_GOALS} 2>&1"
 docker run --rm -it -v /Users/oshoukry/GitHub/openpojo:/openpojo jdk5_openpojo sh -c "$CMD"
