0dd73c7af90f7ebd112398ac5b1f23962a5811f7
oshoukry <oshoukry@openpojo.com>
2018-02-19 21:43:23 -0800

    Adding docker for jdk 5 - 9

--- scripts/jvm_check.sh
+++ scripts/jvm_check.sh
@@ -1,36 +1,23 @@
 #!/bin/sh
 
-function switch_jdk() {
-         local wanted_java_version=$1
-         export JAVA_HOME=`/usr/libexec/java_home -F -v ${wanted_java_version} -a x86_64 -d64`
-        
-         # cleaned PATH
-         export PATH=$(echo $PATH | sed -E "s,(/System)?/Library/Java/JavaVirtualMachines/[a-zA-Z0-9._]+/Contents/Home/bin:,,g")
-        
-         # prepend wanted JAVA_HOME
-         export PATH=${JAVA_HOME}/bin:$PATH
-        
-         echo "Now using : "
-         java -version
-        }
+check_jdk() {
+  VERSION=${1}
+  DOCKER_CONTAINER="jdk${VERSION}_openpojo"
+  CREATED=`docker images -q ${DOCKER_CONTAINER}`
+    if [ -z "${CREATED}" ]; then
+      echo "docker container for jdk${VERSION} not created, creating..."
+      pushd docker
+      ./run.sh rebuild ${VERSION}
+      popd
+    fi
+    rm -rf jdk${VERSION}.results.txt
+    docker/run.sh test ${VERSION} > jdk${VERSION}.results.out
+}
 
-LOGFILE=$0.vm.log
-
-switch_jdk 1.6
-
-pushd ../
-mvn clean package > ${LOGFILE} 2>&1
-tail ${LOGFILE}
-
-switch_jdk 1.7
-mvn clean package > ${LOGFILE} 2>&1
-tail ${LOGFILE}
-
-switch_jdk 1.8
-mvn clean package > ${LOGFILE} 2>&1
-tail ${LOGFILE}
-
-mvn clean
-rm -rf ${LOGFILE}
-
-popd
+if [ -z "${1}" ]; then
+  for COUNTER in {5..9}; do
+    check_jdk ${COUNTER}
+  done
+else
+  check_jdk ${1}
+fi
