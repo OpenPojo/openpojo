b8bf256afd39aebb6235d6ced387f70f83db67e0
Wang <chw6@fa20-cs527-063.cs.illinois.edu>
2020-11-28 11:00:27 -0600

    Update testToString string order

--- src/test/java/com/openpojo/business/BusinessIdentityTest.java
+++ src/test/java/com/openpojo/business/BusinessIdentityTest.java
@@ -142,7 +142,7 @@ public class BusinessIdentityTest {
     final String toString = BusinessIdentity.toString(toStringTestData);
     Assert.assertTrue(String.format("BusinessIdentity.toString() failed!! recieved[%s]", toString),
         toString.startsWith("com.openpojo.business.BusinessIdentityTest$ToStringTestData [@")
-            && toString.endsWith(": instance_name=Instance Name, static_name=Static Name, STATIC_FINAL_NAME=Static Final Name]"));
+            && toString.endsWith(": instance_name=Instance Name, STATIC_FINAL_NAME=Static Final Name, static_name=Static Name]"));
   }
 
   @Test
